4affe4b8ade2f31aadea414ce1435e4659cfca5b
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-25 15:48:28 +0300

    added new files

--- JavaScript-Beginning/startScript.js
+++ JavaScript-Beginning/startScript.js
@@ -1,1 +1,7 @@
-something("Hello!");
+// let let's us to declare a variable
+let total = 149.99;  // as float
+let product = 'Harry Potter book';  //as string
+let booleanExpression = true;  //as boolean
+
+
+something(total+product+booleanExpression);
---
+++ Variables-and-Constants/variables.js
@@ -0,0 +1,27 @@
+// let let's us to declare a variable
+let total = 149.99;  // as float
+let product = 'Harry Potter book';  //as string
+let booleanExpression = true;  //as boolean
+
+// For naming we can use ANYTHING except keywords
+
+// Valid variable names:
+// Starts with One of: _ $ letter
+let _variable = 4;
+let $variable = 5;
+let variable = 6;
+let __proto__ = 4;
+// What starts with __ is probably reserved for something
+
+// Usually variable that starts with _ is private.
+// in c# private readonly string _variable; so in js: let $variable = 4;
+// Usually variable that starts with $ indicates automatically generated variable
+
+// Followeb by Zero or More: _ $ letter number
+
+// CAN NOT START FROM NUMBER AS IN C#
+
+// Constants:
+// In Js we can set constants as: 
+const price = 400;
+//Then we can't change it!
