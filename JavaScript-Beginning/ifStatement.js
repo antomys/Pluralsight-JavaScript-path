197d8d6be656e13e3d63f1a7e04e788bc10d5c4b
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-04-29 00:22:26 +0300

    added ifstatement.js. todo: add truthy and else

---
+++ JavaScript-Beginning/ifStatement.js
@@ -0,0 +1,20 @@
+if(5 === 5) {
+    console.log('5 is equal to 5');
+}
+
+if(5<10){
+    console.log('5 is less than 5');
+}
+
+if(5>=5){
+    console.log('5 is >equal to 5');
+}
+
+let state = 'FL';
+let taxPercent = 7;
+
+if(state !=='FL') {
+    console.log('Tax state is ', Math.random())
+}else{
+    console.log('Tax state is ', taxPercent);
+}
