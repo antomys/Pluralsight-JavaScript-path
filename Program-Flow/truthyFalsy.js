c51ac26055b5b4e17f21ff472e7e5e5012c03bd3
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-05-01 22:37:49 +0300

    added new .js

---
+++ Program-Flow/truthyFalsy.js
@@ -0,0 +1,21 @@
+// Actually falsy are:
+let falsy = [false,0,'empty string',null,undefined,NaN];
+
+console.log('false are:', falsy);
+
+// Truthy are:
+let truthy = [true,0.5,'0'];
+
+console.log('truthy are: ', truthy);
+
+// To round numbers we need to:
+
+// Do not do like that. not fixed!
+if( 1.1 + 1.3 === 2.4)
+    {console.log('NOT FIXED 1.1+1.3 === 2.4');}
+else {console.log('not FIXED 1.1+1.3 !== 2.4');}
+
+// Do like that! .toFixed returns string so we need to convert to number! + sigh does it
+if( +(1.1 + 1.3).toFixed(2) === 2.4)
+    {console.log('FIXED 1.1+1.3 === 2.4');}
+else {console.log('FIXED 1.1+1.3 !== 2.4');}
---
+++ Types-and-Operators/startScript.js
@@ -0,0 +1,14 @@
+// let let's us to declare a variable
+let total = 149.99;  // as float
+let product = 'Harry Potter book';  //as string
+let booleanExpression = true + false;  //as boolean
+
+total = total - 2;
+total -=2;
+total = total % 3 ;
+total%=3;
+total = total / 2;
+total /=2;
+
+
+something(total);
